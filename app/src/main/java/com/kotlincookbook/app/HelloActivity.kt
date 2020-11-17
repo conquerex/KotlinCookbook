b6de1e9688397bf78bcf92862530f3561712f54c
Jongkook <conquerex@gmail.com>
2020-11-17 13:16:28 +0900

    chapter12. 람다를 이용한 클릭 리스너

---
+++ app/src/main/java/com/kotlincookbook/app/HelloActivity.kt
@@ -0,0 +1,26 @@
+package com.kotlincookbook.app
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class HelloActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hello)
+
+        val btn1 = findViewById(R.id.btn1) as Button
+        val btn2 = findViewById(R.id.btn2) as Button
+        val btn3 = findViewById(R.id.btn3) as Button
+
+        btn1.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                Toast.makeText(this@HelloActivity, "Button 1 Click !!!", Toast.LENGTH_SHORT).show()
+            }
+        })
+        btn2.setOnClickListener { Toast.makeText(this@HelloActivity, "Button 2 Click !!!", Toast.LENGTH_SHORT).show() }
+        btn3.setOnClickListener { Toast.makeText(this, "Button 3 Click !!!!!", Toast.LENGTH_SHORT).show() }
+    }
+}
